7212d16c068a37b0f4cef7b85f8f6a873ab9b55b
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-13 18:11:37 -0300

    exercicio via teclado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mariana.arq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        console.readkey();
+        console.readline();
+        string textodigitado=console.readline();
+            Console.write("digite seu nome:")
+            string[mariana]= console.readline();
+            console.write("olá");
+            console.write("mariana");
+            console.writeline("obrigado!");
+            console.writeline("pressione uma tecla para continuar...");
+            console.readkey();
+
+        
+        }
+    }
+}
